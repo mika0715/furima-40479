3535ce5e7b155b4f558cf99241f028c32b055669
mika0715 <162863614+mika0715@users.noreply.github.com>
2024-04-08 11:14:32 +0900

    商品出品機能の途中までと発送までの日数カラムの変更

---
+++ app/models/item.rb
@@ -0,0 +1,22 @@
+class Item < ApplicationRecord
+
+  validates :product, presence: true
+  validates :product_description, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true
+
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
+end
