3d9d47a3045392c2fe070e8f9826c2e7d65e7eae
mika0715 <162863614+mika0715@users.noreply.github.com>
2024-04-09 12:46:00 +0900

    出品機能の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,14 @@
 function cost() {
-
   const priceInput = document.getElementById("item-price");
-    priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = (inputValue * 0.1);
-  const salesProfit = document.getElementById("profit");
-    salesProfit.innerHTML = (inputValue - addTaxDom.innerHTML);
-})
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const taxValue = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = taxValue;
+    const salesProfit = document.getElementById("profit");
+    const profitValue = inputValue - taxValue;
+    salesProfit.innerHTML = profitValue;
+  })
 }
 
 window.addEventListener('turbo:load', cost)
